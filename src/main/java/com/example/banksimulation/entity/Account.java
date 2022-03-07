2f59ed11cca5a9e76124c2bc261978ddfc387239
myizci <izci@hotmail.com>
2022-03-07 12:46:09 -0500

    folders and fields created

--- src/main/java/com/example/banksimulation/entity/Account.java
+++ src/main/java/com/example/banksimulation/entity/Account.java
@@ -1,4 +1,19 @@
 package com.example.banksimulation.entity;
 
+import com.example.banksimulation.enums.AccountType;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
 public class Account {
+    private UUID id;
+    private BigDecimal balance;
+    private AccountType accountType;
+    private Date creationData;
+    private Long userId;
+
+
 }
--- src/main/java/com/example/banksimulation/entity/Transaction.java
+++ src/main/java/com/example/banksimulation/entity/Transaction.java
@@ -1,4 +1,18 @@
 package com.example.banksimulation.entity;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
 public class Transaction {
+    private UUID sender;
+    private UUID receiver;
+    private BigDecimal amount;
+    private String message;
+    private Date creationDate;
+
+
 }
