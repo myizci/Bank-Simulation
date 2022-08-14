509db61909b79cdfd4b815c5482c4a742e27f226
myizci <izci@hotmail.com>
2022-08-14 13:23:48 -0400

    Fixed Transaction entity

--- src/main/java/com/cydeo/banksimulation/model/Transaction.java
+++ src/main/java/com/cydeo/banksimulation/model/Transaction.java
@@ -3,6 +3,7 @@ package com.cydeo.banksimulation.model;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.UUID;
@@ -10,9 +11,16 @@ import java.util.UUID;
 @Data
 @Builder
 public class Transaction {
+    @NotNull
     private UUID sender;
+    @NotNull
     private UUID receiver;
+    @NotNull
+    @Positive
     private BigDecimal amount;
+    @NotEmpty
+    @Size(min = 2, max = 250)
+    @Pattern(regexp = "^[a-zA-Z0-9]*$")
     private String message;
     private Date creationDate;
 
